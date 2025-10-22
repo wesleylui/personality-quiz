201433eca45af5f10d5d18fd54114c4429a1968c
Wesley Lui <wesley.lui2@ucalgary.ca>
2025-10-22 15:46:12 -0600

    Questions init

--- personality-quiz/src/components/Questions.jsx
+++ personality-quiz/src/components/Questions.jsx
@@ -1,2 +1,27 @@
 // Question.jsx
 // hold logic for displaying the questions
+// display each quiz question and its corresponding options
+// we will pass questions, options, and onAnswer as argument to Question.jsx
+// we use JS .map() method to display the question options
+
+import React from "react";
+
+export function Question({ question, options, onAnswer }) {
+  return (
+    <div>
+      <h2>{question}</h2>
+      {options.map(function (option) {
+        return (
+          <button
+            key={option}
+            onClick={function () {
+              onAnswer(option);
+            }}
+          >
+            {option}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
