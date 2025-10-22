800702ddbfaba6c1c04e9ee602d603f807b555bc
Wesley Lui <wesley.lui2@ucalgary.ca>
2025-10-22 15:38:53 -0600

    UserContext init, for name of user

--- personality-quiz/src/components/UserContext.jsx
+++ personality-quiz/src/components/UserContext.jsx
@@ -1,2 +1,17 @@
 // UserContext.jsx
 // will hold the context for the user's name
+
+import React, { createContext, useState } from "react";
+
+// the UserContext
+export const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const [name, setName] = useState("");
+
+  return (
+    <UserContext.Provider value={{ name, setName }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
