dca32c4dd936fa6c928b9f8f2d0d3341897947c7
Wesley Lui <wesley.lui2@ucalgary.ca>
2025-10-23 14:46:30 -0600

    Quiz Result: Results component determines quiz results based on answer. name is also displayed

--- personality-quiz/src/components/Results.jsx
+++ personality-quiz/src/components/Results.jsx
@@ -1,2 +1,28 @@
 // Results.jsx
 // will display the quiz reults alongside the img obtained from API
+
+import React, { useContext } from "react";
+import { UserContext } from "./UserContext";
+
+export default function Results({ element, artwork }) {
+	// reference the context for the "name".
+	const { name } = useContext(UserContext);
+
+	return (
+		<div>
+			<p>
+				<strong>{name}</strong>, your element is: {element}
+			</p>
+			{artwork ? (
+				<div className="artwork">
+					<h2>{artwork.title}</h2>
+					<img src={artwork.primaryImage} alt={artwork.title} />
+					<p>{artwork.artistDisplayName}</p>
+					<p>{artwork.objectDate}</p>
+				</div>
+			) : (
+				<p>No artwork found.</p>
+			)}
+		</div>
+	);
+}
